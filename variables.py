fd60da66b0d8c61ff364bef450487a10aac56cff
Chidiogo <chidiogoeze.me@gmail.com>
2025-07-10 18:25:14 +0100

    some message

--- variables.py
+++ variables.py
@@ -1,1 +1,8 @@
-print("Hello World)
+# Variables and data types in Python. here stings are explictly defined.
+
+character_name = "John" #Here John is a string assigned to the variable "character_name"
+character_age = "35" # "35" here is also a string as it is enclosed.
+print("There once was a man named " + character_name + ", ")
+print("he was 70 years " + character_age + ", ")
+print("he really liked the name " + character_name + ", ")
+print("but he didn't like being " + character_age + "!")
