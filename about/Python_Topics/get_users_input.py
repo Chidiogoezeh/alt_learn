d91c132ca1c8ca2afb731a7c02e8968fa8244409
Chidiogo <chidiogoeze.me@gmail.com>
2025-07-14 11:30:27 +0100

    some message

---
+++ about/Python_Topics/get_users_input.py
@@ -0,0 +1,7 @@
+# Getting input from a user, here a user is allowed to input\
+# required information into our program. We can store this input\
+# inside a variable which we can use. These are ways to make your program more interactive
+
+name = input("Enter your name: ") # Here name is a variable set to input
+age = input("Enter your age: ") # You can add different variables you require from the user 
+print("Hello " + name + "! You are " + age + "years old !") # Run code, python requires you input a value 
