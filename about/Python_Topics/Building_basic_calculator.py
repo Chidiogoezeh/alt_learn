0812dea38a3b3039125159d8b9dc3f4863de7474
Chidiogo <chidiogoeze.me@gmail.com>
2025-07-15 11:37:49 +0100

    some message

---
+++ about/Python_Topics/Building_basic_calculator.py
@@ -0,0 +1,8 @@
+# Create z vsriable where the user will store the number they want to compute. The user can run\
+# diffferent mathematical operations +, -, *, /
+
+num1 = input("Enter a number: ") # Know that python automatically converts user input to a string
+num2 = input("Enter another number: ")
+result = float(num1) + floa(num2) # Use int or float function to convert num to integers or floats
+
+print(result)
---
+++ about/Python_Topics/Mat_lib_game.py
@@ -0,0 +1,5 @@
+# In matlib game, you can use values assigned to variables to change the structure of a story line
+
+print("Malaria is caused by female anopheles mosquito")
+print("Humans and animals can easily get infected when bitten by a mosquito")
+print("Humans can prevent being bitten by killing the mosquito")
