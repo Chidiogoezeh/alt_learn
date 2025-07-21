11f42f9eb476329df10d838ce38eb7aefc2a34b1
Chidiogo <chidiogoeze.me@gmail.com>
2025-07-21 15:46:01 +0100

    some message

--- Python_Topics/Mat_lib_game.py
+++ Python_Topics/Mat_lib_game.py
@@ -1,5 +1,9 @@
 # In matlib game, you can use values assigned to variables to change the structure of a story line
 
-print("Malaria is caused by female anopheles mosquito")
-print("Humans and animals can easily get infected when bitten by a mosquito")
-print("Humans can prevent being bitten by killing the mosquito")
+specie = input("enter a specie: ")
+beings = input("enter a beings: ")
+unaliving = input("enter another word for unaliving: ")
+
+print(f"Malaria is caused by female anopheles " + specie)
+print(f"Humans and " + beings + " can easily get infected when bitten by a mosquito")
+print(f"Humans can prevent being bitten by " + unaliving + " the mosquito")
