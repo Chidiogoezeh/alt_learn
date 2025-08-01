5627c78c312f51b448cee7614e55a52b6da54713
Chidiogo <chidiogoeze.me@gmail.com>
2025-08-01 12:56:04 +0100

    some messsage

---
+++ Python_Topics/11_Function.py
@@ -0,0 +1,10 @@
+# Function is a collection of code whih performs a pscific task
+def say_hi(name, age):
+    print("Hello User" + name + ", you are" + age + "years old")
+
+print("Top") # Executes first
+say_hi() # Executes second
+print("Bottom") # Executes third
+
+say_hi("Kuke", "35") # Ascribe parameters to the function
+say_hi("Nkit", "42")
