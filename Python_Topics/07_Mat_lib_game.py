6831564e5c46b4e1dc8e584317be8d264d6e07d9
Chidiogo <chidiogoeze.me@gmail.com>
2025-08-03 11:00:07 +0100

    some messasge

--- Python_Topics/07_Mat_lib_game.py
+++
@@ -1,9 +0,0 @@
-# In matlib game, you can use values assigned to variables to change the structure of a story line
-
-specie = input("enter a specie: ")
-beings = input("enter a beings: ")
-unaliving = input("enter another word for unaliving: ")
-
-print(f"Malaria is caused by female anopheles " + specie)
-print(f"Humans and " + beings + " can easily get infected when bitten by a mosquito")
-print(f"Humans can prevent being bitten by " + unaliving + " the mosquito")
--- Python_Topics/10_Tuples.py
+++
@@ -1,3 +0,0 @@
-# Tuples are immutable
-cordinates = (4, 5)
-print(cordinates[0])
